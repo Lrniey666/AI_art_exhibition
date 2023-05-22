86deffe0407f6c69b3ba61adf91f55de4ac387c6
ejiru4u3 <sgabc524567@gmail.com>
2023-05-22 13:38:43 +0800

    首頁完成

--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests, json
 from mysite import models           # 匯入 mysite 資料夾底下 models.py 中所有的類別
+import random   #匯入亂數模組
 
 def index(request):
-    myname = ["第十組"]
+    mynames = ["第十組"]
+    myname = random.choice(mynames)
     return render(request, "index.html", locals())
