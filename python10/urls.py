2e4dcbf3ea032f1fb69c504cbaab11e58132752c
ejiru4u3 <sgabc524567@gmail.com>
2023-06-17 16:56:31 +0800

    頂部圖更新，以及全部網頁美化完成

--- python10/urls.py
+++ python10/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from mysite import views   # 引入views.py中所有的函式
 
-
 urlpatterns = [
     path('', views.index, name='index'),      # add a name to the URL pattern
     path('admin/', admin.site.urls),
