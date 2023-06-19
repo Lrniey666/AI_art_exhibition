dba8f00e991d2ed53f0d412abf7666cd78fb3ba4
Lrniey666 <iancear930616@gmail.com>
2023-06-19 19:58:41 +0800

    張任沂的個人AI畫展

--- mysite/views.py
+++ mysite/views.py
@@ -4,10 +4,7 @@ from django.http import HttpResponse
 import requests, json
 from mysite import models           # 匯入 mysite 資料夾底下 models.py 中所有的類別
 import random   #匯入亂數模組
-from api_fetchers.household_vehicle_fetcher import fetch_and_store_vehicle_data as fasvd
-from api_fetchers.household_income_fetcher import fetch_and_store_income_data as fasid
-from api_fetchers.UACSS_fetcher import fetch_and_store_UACSS_data as fasuad
-from api_fetchers.Population_stats_fetcher import fetch_and_store_Population_stats_data as faspsd
+
 from Data_masage.vehicle_quantity_ranking import get_new_vehicle as gnv,get_vehicle_latest_year_month as gvlyrm, vehicle_bus_ranking, vehicle_car_ranking,vehicle_ranking as vr, vehicle_sc_ranking, vehicle_truck_ranking
 from Data_masage.six_city_vehicle_incom_compare import get_six_city_car_data_as_income_renew_year as sccair,get_six_city_scooter_data_as_income_renew_year as scsair, get_new_Household_income as gnhi,\
                                                     get_six_city_vehicle_data_as_income_renew_year as scvair,get_six_city_new_population_quantity_vic as npqv,find_old_time_vic as fotv,get_six_city_population_avg_vehicle,get_six_city_month_PCI,get_six_city_population_avg_car,get_six_city_population_avg_scooter
@@ -267,4 +264,17 @@ def student_and_vehicle_car(request):
 
     return render(request, 'student_and_vehicle_car.html', locals())
     print(vr())
+
+def druck_butty(request):
+
+    return render(request, 'druck_butty.html')
+
+def window_butty(request):
+
+    return render(request, 'window_butty.html')
+
+def TV_butty(request):
+
+    return render(request, 'TV_butty.html')
+
 
